bcc99187a172aac157515eaf3a76220ee077317f
Mert Erdogan <merterdoogan@gmail.com>
2019-02-01 15:12:52 +0300

    Validasyon işlemleri

--- models/Movie.js
+++ models/Movie.js
@@ -4,12 +4,30 @@ const Schema = mongoose.Schema;
 const MovieSchema = new Schema({
     title: {
         type: String,
-        required: true
+        required: [true, '`{PATH}` alanı zorunludur.'],
+        maxlength: [15, '`{PATH}` alanı (`{VALUE}`), ({MAXLENGTH}) karakterdan fazla olamaz'],
+        minlenght: 1,
     },
-    category: String,
-    country: String,
-    year: Number,
-    imdb_scrore: Number,
+    category: {
+        type: String,
+        maxlength: 30,
+        minlength: 1,
+    },
+    country: {
+        type: String,
+        maxlength: 30,
+        minlength: 4,
+    },
+    year: {
+        type: Number,
+        max: 2040,
+        min: 1900
+    },
+    imdb_scrore: {
+        type: Number,
+        max: 10,
+        min: 0
+    },
     createdAt: {
         type: Date,
         default: Date.now 
