7f3be26d5a75a32d51a2a66757863331849b4766
Mert Erdogan <merterdoogan@gmail.com>
2019-02-01 16:26:19 +0300

    users

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,30 @@
 const express = require('express');
 const router = express.Router();
 
+
+//Models
+const Users = require('../models/Users.js');
+
 /* GET home page. */
 router.get('/', (req, res, next) => {
   res.render('index', { title: 'Express' });
+});
+
+router.post('/register', (req, res, next) => {
+  const { username, password } = req.body;
+  
+  const user = new Users({
+    username,
+    password
+  });
+
+  const promise = user.save();
+
+  promise.then((data) => {
+    res.json(data);
+  }).catch((err) => {
+    res.json(err);
+  });
 });
 
 module.exports = router;
